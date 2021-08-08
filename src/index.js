b98036095127fdc50c831589989eaac82bb19861
wassaybajwa <wassaybajwa@hotmail.com>
2021-08-08 02:15:57 -0400

    Managing React complex state with change in index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+// const citrus = ["apple", "lime", "orange"]
+// const fruits = ["banana", ...citrus, "peach", "grapes"]
+
+// console.log(fruits)
+
+
+
+// const fullname =  {
+
+//   fname: "james",
+//   lname: "bond"  
+// }
+
+// const user = {
+//   ...fullname,
+//   id: 1,
+//   email: "was@fo.com"
+// }
+
+// console.log(user)
